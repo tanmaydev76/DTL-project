e731d7f05a765285edde531394a9601a76f5d8e2
tanmaydev76 <tanmaykamble3107@gmail.com>
2026-04-16 11:31:24 +0530

    add & operations

---
+++ operations.h
@@ -0,0 +1,9 @@
+#ifndef OPERATIONS_H
+#define OPERATIONS_H
+
+double add(double a, double b);
+double sub(double a, double b);
+double mul(double a, double b);
+double divide(double a, double b);
+
+#endif
